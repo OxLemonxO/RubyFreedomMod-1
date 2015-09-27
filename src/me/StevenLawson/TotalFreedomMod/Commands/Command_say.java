849d7ae5d8b4f4b6f39d1e36030707f8ebcd04d0
reuben4545 <reuben4545@users.noreply.github.com>
2015-09-27 00:02:38 -0700

    Update Command_say.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_say.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_say.java
@@ -18,13 +18,7 @@ public class Command_say extends TFM_Command
         {
             return false;
         }
-        
-        if (senderIsConsole)
-        {
-            TFM_Log.info("Don't spam you prick!");
-            return true;
-        }
-
+       
         String message = StringUtils.join(args, " ");
 
         if (senderIsConsole && TFM_Util.isFromHostConsole(sender.getName()))
