69588d77589cb6c68dd1b2e5d07266dadcef50ce
DarkGamingDronze <wesam_111@hotmail.com>
2015-09-30 06:35:32 -0700

    Update TFM_Util.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -71,12 +71,11 @@ public class TFM_Util
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
     public static final List<String> FOP_DEVELOPERS = Arrays.asList("Paldiu", "xDestroyer217", "Freelix2000", "Cyro1999"); // this isn't used, but we are keeping it for credit reasoning
-    public static final List<String> RF_DEVELOPERS = Arrays.asList("Hockeyfan360");
-    public static final List<String> EXECUTIVES = Arrays.asList("Alosion", "DF_Crafted", "ItzTrae");
+    public static final List<String> RF_DEVELOPERS = Arrays.asList("Hockeyfan360", "dsate1");
+    public static final List<String> EXECUTIVES = Arrays.asList("Alosion", "DF_Crafted", "ItzTrae", "GLa_DOS_");
     public static final List<String> COCFD = Arrays.asList("Joenmb");
-    public static final List<String> SYS = Arrays.asList("DarkHorse108", "xYurippe", "cowgomooo12", "MysteriAce", "SupItsLuka", "dlg666999", "Joenmb", "eddieusselman", "ItsFenixMC");
-    public static final List<String> SPECIALISTS = Arrays.asList("reuben4545");
-    public static final List<String> COOWNER = Arrays.asList("TaahThePenguin", "AndySixx", "decyj145");
+    public static final List<String> SYS = Arrays.asList("DarkHorse108", "xYurippe", "cowgomooo12", "MysteriAce", "OxLemonxO", "ItsFenixMC");
+    public static final List<String> COOWNER = Arrays.asList("TaahThePenguin", "AndySixx", "DarkGamingDronze");
     public static final List<String> LEADDEV = Arrays.asList("iDelRey");
     private static final Random RANDOM = new Random();
     public static String DATE_STORAGE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
